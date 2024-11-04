68ac9f70e97f96b530c3e0c9f3f204b8b101d1dd
Alperen Altuntaş <altuntas5732@gmail.com>
2024-11-04 11:16:16 +0300

    Serializing

--- RPG Project/Assets/Scripts/Saving/SavingSystem.cs
+++ RPG Project/Assets/Scripts/Saving/SavingSystem.cs
@@ -10,16 +10,27 @@ namespace RPG.Saving
         {
             string path = GetPathFromSaveFile(saveFile);
             print("Saving to " + path);
-            FileStream stream = File.Open(path,FileMode.Create);
-            byte[] byteArray = Encoding.UTF8.GetBytes("Hello World");
-            stream.Write(byteArray, 0, byteArray.Length);
-            stream.Close();
+            using (FileStream stream = File.Open(path, FileMode.Create))
+            {
+                byte[] byteArray = Encoding.UTF8.GetBytes("Hello World");
+                stream.Write(byteArray, 0, byteArray.Length);
+            }
         }
 
         public void Load(string saveFile)
         {
+            string path = GetPathFromSaveFile(saveFile);
             print("Loading from " + GetPathFromSaveFile(saveFile));
+            using (FileStream stream = File.Open(path, FileMode.Open))
+            {
+                byte[] buffer = new byte[stream.Length];
+                stream.Read(buffer, 0, buffer.Length);
+            
+                string message = Encoding.UTF8.GetString(buffer);
+                print(message);
+            }
         }
+        
 
         private string GetPathFromSaveFile(string saveFile)
         {
